4af5179f2e9436c479122b78e053f015ec06ee27
MeenaStalin <53248447+MeenaStalin@users.noreply.github.com>
2019-07-24 14:33:40 +0530

    Create plays23.py

---
+++ plays23.py
@@ -0,0 +1,7 @@
+ab,bs=map(int,input().split(" "))
+a123=list(map(int,input().split(" ")))
+a234=list(map(int,input().split(" ")))
+for i in a234:
+    a123.insert(0,i)
+    c3=max(a123)
+    print(c3,end=" ")
